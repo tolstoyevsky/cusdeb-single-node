9aa7c325a971eac0cee331ff710defa9f7ae5691
Evgeny Golyshev <eugulixes@gmail.com>
2020-05-06 00:59:14 +0300

    Make BM write to blackmagic.log when starting
    Closes #21.

--- runners/run_bm.sh
+++ runners/run_bm.sh
@@ -15,5 +15,5 @@ pushd "${TARGET}/blackmagic"
         --mongodb-port="${MONGO_PORT}" \
         --port="${BM_PORT}" \
         --token-key="${TOKEN_KEY}" \
-        --logging=debug
+        --logging=debug 2>> "${TARGET}"/blackmagic.log
 popd
