05d68ac78094546112e111af3a57c7f47ad3dbac
Evgeny Golyshev <eugulixes@gmail.com>
2020-05-06 01:27:24 +0300

    Change --base-systems-path value

--- runners/run_bm.sh
+++ runners/run_bm.sh
@@ -7,7 +7,7 @@ pushd "${TARGET}/blackmagic"
     export PYTHONPATH
 
     "${TARGET}"/blackmagic-env/bin/python bin/server.py \
-        --base-systems-path="${TARGET}" \
+        --base-systems-path="${TARGET}"/chroots \
         --dominion-workspace="${TARGET}"/dominion-workspace \
         --log-file-prefix="${TARGET}"/blackmagic.log \
         --max-builds-number=1 \
