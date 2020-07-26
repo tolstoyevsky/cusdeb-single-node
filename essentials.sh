22e975440b866e0f9c360425a771dd30904a8ded
Evgeny Golyshev <eugulixes@gmail.com>
2020-07-26 18:11:25 +0300

    Upgrade Node.js to 12.18.3

--- essentials.sh
+++ essentials.sh
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # shellcheck disable=SC2034
-NODE_VER=8.12.0
+NODE_VER=12.18.3
 
 # shellcheck disable=SC2034
 PIP_VER=20.0.2
