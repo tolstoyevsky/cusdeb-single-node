2d4f0658ac0aa0d1afea99d43494a2a37e5985f3
Evgeny Golyshev <eugulixes@gmail.com>
2020-07-29 20:06:24 +0300

    Use supervisord logging facilities

--- runners/run_cusdeb_api.sh
+++ runners/run_cusdeb_api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd "${TARGET}/cusdeb-api"
-    "${TARGET}"/cusdeb-api-env/bin/python manage.py runserver 0.0.0.0:"${CUSDEB_API_PORT}" &>> "${TARGET}"/cusdeb-api.log
+    "${TARGET}"/cusdeb-api-env/bin/python manage.py runserver 0.0.0.0:"${CUSDEB_API_PORT}"
 popd
