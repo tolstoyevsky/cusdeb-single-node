d934612fdd0ce241e3ba267e3aff6f0ccc8907d0
Evgeny Golyshev <eugulixes@gmail.com>
2020-10-24 18:37:14 +0300

    Run dominion beat and worker on 'start'

---
+++ runners/run_dominion_beat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd "${TARGET}/dominion"
+    PYTHONPATH="${TARGET}"/cusdeb-api:"$(pwd)"
+    export PYTHONPATH
+
+    export C_FORCE_ROOT=1
+
+    export DJANGO_SETTINGS_MODULE=dominion.settings
+
+    "${TARGET}"/dominion-env/bin/celery -A dominion.tasks worker -B -Q beat --loglevel=fatal
+popd
+
