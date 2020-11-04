17645e8c818d09a7214795f914b2a75b99268c15
Evgeny Golyshev <eugulixes@gmail.com>
2020-11-04 19:49:01 +0300

    Reflect latest changes in Dominion

--- runners/run_dominion.sh
+++ runners/run_dominion.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 pushd "${TARGET}/dominion"
-    PYTHONPATH="${TARGET}"/dominion:"${TARGET}"/shirow:"$(pwd)"
+    PYTHONPATH="${TARGET}"/dominion:"${TARGET}"/cusdeb-api:"${TARGET}"/shirow:"$(pwd)"
     export PYTHONPATH
 
-    "${TARGET}"/dominion-env/bin/python bin/server.py \
-        --build-log-dir="${TARGET}"/dominion-workspace \
+    env DJANGO_SETTINGS_MODULE=dominion.settings "${TARGET}"/dominion-env/bin/python bin/server.py \
         --log-file-prefix="${TARGET}"/dominion.log \
         --port="${DOMINION_PORT}" \
         --token-key="${SECRET_KEY}" \
