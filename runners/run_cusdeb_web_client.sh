2f3199847439c03fe52104fe9d1f0e642b256bcc
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-04-14 14:46:05 +0300

    Pass BUILD_RESULT_URL to cusdeb-web-client

--- runners/run_cusdeb_web_client.sh
+++ runners/run_cusdeb_web_client.sh
@@ -5,6 +5,7 @@ pushd "${TARGET}/cusdeb-web-client"
         env PATH="${TARGET}"/node/bin:"${PATH}" \
             NODE_PATH="${TARGET}"/node/lib/node_modules \
             \
+            BUILD_RESULT_URL=http://localhost:"${BUILD_RESULT_PORT}" \
             CUSDEB_TZ_URL=http://localhost:"${TZ_PORT}" \
             CUSDEB_HELPIK_URL=http://localhost:"${HELPIK_PORT}" \
             CUSDEB_API_URL=http://localhost:"${CUSDEB_API_PORT}" \
